abaa8c43243dc1a1ddcc7c767a7c7b0666e09460
Jay Patel <46022232+jaypatel-13@users.noreply.github.com>
2020-02-13 13:21:15 +0530

    Added Tiff2Raw Conversion
    Co-Authored-By: Mohit Mandlewala <mandlewalamohit@gmail.com>
    Co-Authored-By: nkush1710 <nkush1710@users.noreply.github.com>

---
+++ Assets/Scripts/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Diagnostics;
+
+public class Converter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void runFile()
+    {
+        Process.Start(Environment.CurrentDirectory + @"\Tiff2Raw.exe");
+    }
+}
---
+++ Assets/Scripts/WaterLevel.cs
@@ -0,0 +1,67 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class WaterLevel : MonoBehaviour
+// {
+//     public float climbSpeed = 4;
+//     public Plane waterPlane;
+//     private Vector3 waterSize;
+//     public InputField inputLevel;
+//     private bool changeh = false;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         waterSize = waterPlane.size;
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.GetKey (KeyCode.KeypadPlus)) {transform.position += transform.up * climbSpeed * Time.deltaTime;}
+// 		if (Input.GetKey (KeyCode.KeypadMinus)) {transform.position -= transform.up * climbSpeed * Time.deltaTime;}
+//     }
+
+//     public void changeWaterLevel()
+//     {
+
+//     }
+// }
+
+// public Terrain terrain;
+//     private Vector3 terrainSize;
+//     public InputField inputHeight;  
+    
+//     private bool changeh = false;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         terrainSize = terrain.terrainData.size;
+//     }
+
+//     // // Update is called once per frame
+//      void Update()
+//      {
+//          if(changeh)
+//          {
+//              float h;
+//              if(inputHeight.text.Length == 0)
+//              {
+//                  h = 200f;
+//              }
+//              else
+//              {
+//                 h = float.Parse(inputHeight.text);
+//              }
+//             terrain.terrainData.size = new Vector3(terrainSize.x , h , terrainSize.z);
+//             changeh = false;
+//          }
+         
+//     }
+
+//     public void ChangeHeight()
+//     {
+//         changeh = true;
+//     }
