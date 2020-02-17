018f996076282c797b120577e1ef9aead70410a2
Mohit <mandlewalamohit@gmail.com>
2020-02-17 11:44:12 +0530

    revert

---
+++ Assets/Scripts/FullScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour
+{
+    public void fullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+}
